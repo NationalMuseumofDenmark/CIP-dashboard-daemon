3e1919d103a2631af997793ac9bc9f77c3079765
Kræn Hansen <kh@bitblueprint.com>
2014-01-13 16:31:15 +0100

    Fixing Malthes bugs.

--- plugins/value_transformers/BoolTransformer.php
+++ plugins/value_transformers/BoolTransformer.php
@@ -4,7 +4,9 @@ require_once 'plugins/IValueTransformer.php';
 
 class BoolTransformer implements IValueTransformer {
   public function transform($bool) {
-    assert('is_bool($bool)') || throw new \InvalidArgumentException('BoolTransformer must be passed a boolean value');
+    if(assert('is_bool($bool)')) {
+      throw new \InvalidArgumentException('BoolTransformer must be passed a boolean value');
+    }
     if ($bool === TRUE) {
       return 'TRUE';
     } else {
