4dcf5d971ddf7a28d9b4a8bd3c1cb3a34b44e1ba
Kræn Hansen <kh@bitblueprint.com>
2014-01-13 16:32:16 +0100

    Woups - Fixing my own bug.

--- plugins/value_transformers/BoolTransformer.php
+++ plugins/value_transformers/BoolTransformer.php
@@ -4,7 +4,7 @@ require_once 'plugins/IValueTransformer.php';
 
 class BoolTransformer implements IValueTransformer {
   public function transform($bool) {
-    if(assert('is_bool($bool)')) {
+    if(!assert('is_bool($bool)')) {
       throw new \InvalidArgumentException('BoolTransformer must be passed a boolean value');
     }
     if ($bool === TRUE) {
